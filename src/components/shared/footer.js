f9154eb0f4534b2d9c9d639f0ff4816418586284
Ken Pink <kenpink@gmail.com>
2021-03-05 16:41:58 -0500

    ken work so far

--- src/components/shared/footer.js
+++ src/components/shared/footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Container } from 'reactstrap'
 
+
 const Footer = () => {
     return(
-    <footer className="py-5 bg-primary">
+    <footer id="gradient" className="py-5 bg-primary" >
         <Container>
             <p className="m-0 text-center text-white">Copyright &copy; Ken Pink 2021</p>
         </Container>
